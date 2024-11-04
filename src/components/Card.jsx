1497c1520c1f584436e8d7012db821bb867089e3
hje <hjerikson@gmail.com>
2024-11-04 08:16:36 +0100

    CardContainer now shows cards in random order each time

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
-export default function Card() {
+export default function Card({ id, source }) {
   return (
     <div className="w-64 h-64 bg-slate-400 p-4 rounded-xl m-4 hover:bg-slate-300 active:bg-slate-400 hover:cursor-pointer select-none">
-      <img src="src/assets/gifs/1.webp" />
+      {/* <img src="src/assets/gifs/1.webp" /> */}
+      <img className="w-full h-full object-cover" src={source} />
     </div>
   );
 }
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,20 +1,38 @@
+// Components
 import Card from "./Card";
+// Pull in global state
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export default function CardContainer() {
+  // Get cards from global state
+  const { cards } = useContext(GlobalContext);
+  
+  // Shuffle card order and store it in cardOrder
+  const shuffledCards = shuffleCards([...cards]); // Spread operator to create a copy
+
   return (
     <div className="flex flex-wrap justify-evenly mt-32 my-8">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {shuffledCards.map((card) => (
+        <Card key={card.id} source={card.image} />
+      ))}
+
     </div>
   );
+}
+
+function shuffleCards(array) {
+  let currentIndex = array.length
+
+  // While there remain elements to shuffle...
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    let randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
 }

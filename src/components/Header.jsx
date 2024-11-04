38cae024f17b7a16ef2dfaa35f18c5506026ff75
hje <hjerikson@gmail.com>
2024-11-04 08:55:42 +0100

    Add a pulse animation to the score on update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 // Pull in global state
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 export default function Header() {
   const { score, bestScore } = useContext(GlobalContext);
+
+  const [pulseKey, setPulseKey] = useState(0);
+  // Update pulse key whenever score changes
+  useEffect(() => {
+    setPulseKey(Date.now()); // Set a unique key based on timestamp
+  }, [score]);
 
   return (
     <header className="fixed top-0 z-50 w-full flex items-center justify-between bg-slate-700 py-8 px-4 border-b-2 border-b-slate-400 shadow-black">
@@ -12,7 +18,7 @@ export default function Header() {
         <p>Click on an image, but not more than once!</p>
       </div>
       <div>
-        <h2 className="text-4xl bg-orange-700 py-2 px-4 rounded">Score: <span>{score}</span></h2>
+        <h2 key={pulseKey} className="text-4xl bg-orange-700 py-2 px-4 rounded pulse-animation">Score: <span>{score}</span></h2>
       </div>
       <div>
         <h3 className="text-2xl bg-slate-800 py-2 px-4 rounded">Best score: <span>{bestScore}</span></h3>
